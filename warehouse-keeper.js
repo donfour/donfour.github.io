3057d69f12f7ccfa3a24c318c1812cc054f557fe
Donovan So <donovanso@yahoo.com.hk>
2017-09-23 22:31:38 +0800

    https
    https

--- warehouse-keeper.js
+++ warehouse-keeper.js
@@ -28,28 +28,28 @@ $(document).ready(function(){
 
     if (e.which == 119) { // up arrow
       console.log('pressed up');
-      axios.post(`http://cis2017-warehouse-keeper.herokuapp.com/move/up?run_id=${runid}`)
+      axios.post(`https://cis2017-warehouse-keeper.herokuapp.com/move/up?run_id=${runid}`)
       .then(function(resp){
         console.log('resp', resp.data);
         renderpage(resp.data)
       })
     } else if (e.which == 115) { // down arrow
       console.log('pressed down');
-      axios.post(`http://cis2017-warehouse-keeper.herokuapp.com/move/down?run_id=${runid}`)
+      axios.post(`https://cis2017-warehouse-keeper.herokuapp.com/move/down?run_id=${runid}`)
       .then(function(resp){
         console.log('resp', resp.data);
         renderpage(resp.data)
       })
     } else if (e.which == 97) { // left arrow
       console.log('pressed left');
-      axios.post(`http://cis2017-warehouse-keeper.herokuapp.com/move/left?run_id=${runid}`)
+      axios.post(`https://cis2017-warehouse-keeper.herokuapp.com/move/left?run_id=${runid}`)
       .then(function(resp){
         console.log('resp', resp.data);
         renderpage(resp.data)
       })
     } else if (e.which == 100) { // right arrow
       console.log('pressed right');
-      axios.post(`http://cis2017-warehouse-keeper.herokuapp.com/move/right?run_id=${runid}`)
+      axios.post(`https://cis2017-warehouse-keeper.herokuapp.com/move/right?run_id=${runid}`)
       .then(function(resp){
         console.log('resp', resp.data);
         renderpage(resp.data)
